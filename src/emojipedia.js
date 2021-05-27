28f2e2d7e4a5001d2027892570ca0502002aefcc
Shaishav Rastogi <rastogishaishav@gmail.com>
2021-05-27 20:49:19 +0530

    First

---
+++ src/emojipedia.js
@@ -0,0 +1,35 @@
+const emojipedia = [
+    {
+        id: 1,
+        emoji : "💪",
+        mane: "Tense Biceps",
+        meaning :
+        " 'You can do that!' or 'I feel Strong!' Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym" 
+    },
+    
+    {
+        id: 2,
+        emoji : "🥰",
+        mane: "Smiling face with smiling eyes and three hearts",
+        meaning :
+        "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart. "
+    },
+    
+    {
+        id: 3,
+        emoji : "😘",
+        mane: "Face Throwing a Kiss",
+        meaning :
+        "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude. "
+    },
+    
+    {
+        id: 4,
+        emoji : "🤓",
+        mane: "Nerd Face",
+        meaning :
+        "Huge glasses, awkward smile and buck teeth. Used humorously or ironically for nerds or to express how smart you are. Stereotype of a nerd; a smart but funny-dressed person with social deficits."
+    },
+];
+
+export default emojipedia;
